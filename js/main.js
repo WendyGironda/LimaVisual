1ab2a2f61d4e57cd3eae36ae6a9169edd0a652fd
Martin <martin.rios.reynoso@gmail.com>
2017-11-29 06:57:02 -0500

    mapa de google y logos de auspiciadores

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
+	// $('.cycle-slideshow').cycle({
+	// 	slides:"> li",
+ //    	speed: 600,
+ //    	manualSpeed: 100,
+ //    	prev:"#prev",
+	// 	next:"#next",
+	// 	fx:"scrollHorz",
+	// 	startingSlide:"2"
+	// });
+
 	$(".inscribete__left, .inscribete__right").on("click", function(){
 		$(this).parent().find(".inscribete__center").removeClass("inscribete__center--collapsed");
 		$(this).parent().find(".inscribete__right").fadeOut();
-	})
+	});
 })
